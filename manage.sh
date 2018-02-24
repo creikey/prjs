e59a860c532ee3ee60d70c03741e253a20e5b55d
Cameron Reikes <cameronreikes@gmail.com>
2018-02-24 01:04:24 -0800

    Add updating function

--- manage.sh
+++ manage.sh
@@ -7,7 +7,34 @@ is_installed() {
 }
 
 if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
-	echo -e -n "--Manages prjs--\n--install : installs to .bashrc\n--uninstall : uninstalls from the bashrc\n"
+	echo -e -n "--Manages prjs--\n--install : installs to .bashrc\n--uninstall : uninstalls from the bashrc\n--update : pulls from git to update\n"
+elif [ "$1" == "--update" ]; then
+	echo -e -n "Updating..."
+	if [ ! -d ".git" ]; then
+		echo -e -n "FAIL!\n"
+		echo -e -n "-Not a git repository, cannot pull!\n"
+	else
+		REMOTES=$(git remote -v | grep origin)
+		if [ "$REMOTES" == "" ]; then
+			echo -e -n "FAIL!\n"
+			echo -e -n "-No origin remote to pull from!\n"
+			echo -e -n "-ABORT - OUT OF OPTIONS\n"
+			exit
+		fi
+		GITSTATUS=$(git status -s)
+		if [ "$GITSTATUS" != "" ]; then
+			echo -e -n "FAIL!\n"
+			echo -e -n "-Local changes are not yet saved! Overwrite them? "
+			read -n1 ans
+			if [ "$ans" != "y" ]; then
+				echo -e -n "-ABORT - OUT OF OPTIONS\n"
+				exit
+			fi
+		fi
+		echo -e -n "OK!\n\n"
+		git fetch --all
+		git reset --hard origin/master
+	fi
 elif [ "$1" == "--install" ]; then
 	if [ "$(is_installed)" == "" ]; then
 		echo -e -n "Prjs not installed...\n"
